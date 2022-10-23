d47fdfb1c424cfabc540820a1fdd8b50211b7895
Alen4ick <karapetian30@mail.ru>
2022-10-23 15:34:47 +0300

    n

--- yandex_alg_1/lectures/Lecture_3/d_contest.py
+++ yandex_alg_1/lectures/Lecture_3/d_contest.py
@@ -1,13 +1,9 @@
-import sys
-all_lines = sys.stdin
+
+f = open('input.txt', 'r')
+lines = f.readlines()
 my_set = set()
-for line in all_lines:
-    if line.strip() == "":
-        break
-    line = line.strip()
-    m = [x for x in line[0:len(line) -1].split()]
-    for el in m:
-        if el not in my_set:
-            my_set.add(el)
+for line in lines:
+    for el in line.split():
+        my_set.add(el)
 print(len(my_set))
-print(my_set)
+f.close()
