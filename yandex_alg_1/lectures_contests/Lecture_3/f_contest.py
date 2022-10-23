fa4f11f71baff750bb38c25523b5aab8911e03f8
Alen4ick <karapetian30@mail.ru>
2022-10-23 22:20:03 +0300

    d

--- yandex_alg_1/lectures_contests/Lecture_3/f_contest.py
+++ yandex_alg_1/lectures_contests/Lecture_3/f_contest.py
@@ -1,1 +1,9 @@
-str_1, str_2 = input(), input()
+line_1, line_2 = input(), input()
+my_set = set()
+for i in range(len(line_2) - 1):
+    my_set.add(line_2[i:i+2])
+counter = 0
+for i in range(len(line_1) - 1):
+    if line_1[i:i+2] in my_set:
+        counter +=1
+print(counter)
